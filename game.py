6820fdba311ad1a95d8d2feab88db58811e64cf1
kagaradata <137279126+kagaradata@users.noreply.github.com>
2024-12-05 08:19:37 +0500

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import random
 
 def hangman(word):
     wrong = 0
+    
     stages = ["",
               "__________     ",
               "|        |     ",
@@ -11,28 +12,34 @@ def hangman(word):
               "|       / \    ",
               "|              "
               ]
+    
     rletters = list(word)
     board = ["_"] * len(word)
     win = False
     print("start")
+    
     while wrong < len(stages) - 1:
         print("\n")
         msg = "name the letter: "
         char = input(msg)
+        
         if char in rletters:
             cind = rletters.index(char)
             board[cind] = char
             rletters[cind] = '$'
+            
         else:
             wrong += 1
         print((" ".join(board)))
         e = wrong + 1
         print("\n".join(stages[0: e]))
+        
         if "_" not in board:
             print("you win")
             print(" ".join(board))
             win = True
             break
+            
     if not win:
         print("\n".join(stages[0: wrong]))
         print("you lose! word: {}.".format(word))
